b600c180dd5e151bab6b5f031b9b7af2e7ed34f2
Vladimir Sotnikov <vladimir.sotnikov@jetbrains.com>
2023-09-26 16:03:15 +0300

    set wildcard for CORS origins

--- src/api/main.py
+++ src/api/main.py
@@ -103,13 +103,10 @@ class FilterParameters(BaseModel):
 
 
 app = FastAPI()
-origins = [
-    "http://localhost:3000"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
