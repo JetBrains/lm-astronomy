7c17146e75f6af79dc865e576ea9a0c3572fd4b6
Vladimir Sotnikov <vladimir.sotnikov@jetbrains.com>
2023-09-26 17:10:55 +0300

    fix search

--- src/front/src/components/MessengerType/MessengerType.js
+++ src/front/src/components/MessengerType/MessengerType.js
@@ -3,10 +3,10 @@ import './MessengerType.css';
 
 function MessengerType(props) {
     const messengers = [
-        { label: 'γ', name: 'gamma' },
-        { label: 'ν', name: 'nu' },
-        { label: 'G', name: 'bigG' },
-        { label: 'p', name: 'smallP' }
+        { label: 'γ', name: 'Electromagnetic Radiation' },
+        { label: 'ν', name: 'Neutrinos' },
+        { label: 'G', name: 'Gravitational Waves' },
+        { label: 'p', name: 'Cosmic Rays' }
     ];
 
     const [selectedMessenger, setSelectedMessenger] = useState(null);
