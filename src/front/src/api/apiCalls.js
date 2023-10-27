6e13a94d82e1fdc3d216d1e4505068a4f089b43e
Sergo <serg@jetbrains.com>
2023-10-27 12:49:06 +0200

    coords and radius add in url corectly now

--- src/front/src/api/apiCalls.js
+++ src/front/src/api/apiCalls.js
@@ -1,39 +1,55 @@
-export function searchAPI(transientName, physicalPhenomena, messengerType) {
-    // Base URL for the search API. TODO: Consider moving to an environment variable.
-    const baseURL = 'https://lm-astronomy.labs.jb.gg/api/search/';
+import { parseAndCleanCoordinates } from '../components/parseCoordinatesUtility.js';
 
-    // Headers for the request.
+export function searchAPI(transientName, physicalPhenomena, messengerType) {
+    const baseURL = 'https://lm-astronomy.labs.jb.gg/api/search/';
     const headers = {
         'accept': 'application/json'
     };
 
-    // Convert empty inputs to null and construct the URL.
+    const coordinates = parseAndCleanCoordinates(transientName);
+
     function constructURL(base, params) {
-        // Map through parameters and set to null if not provided.
+        let coordinatesString = '';
+        if (params.ra !== null && params.dec !== null) {
+            coordinatesString = `${params.ra} ${params.dec}`;
+        }
+        if (params.transient_name) {
+            coordinatesString = `${coordinatesString} ${params.transient_name}`.trim();
+        }
+
+        // Replace the old parameters with the new format
+        delete params.ra;
+        delete params.dec;
+        delete params.transient_name;
+        params.object_name_or_coordinates = coordinatesString;
+
+        // Map through parameters and set to null if not provided
         const filteredParams = Object.entries(params)
             .map(([key, value]) => [key, value || null])
             .filter(([key, value]) => value !== null);
 
-        // Return base URL if no valid parameters are provided.
+        // Return base URL if no valid parameters are provided
         if (!filteredParams.length) {
             return base;
         }
 
-        // Convert parameters to a query string and replace '+' with '%20'.
+        // Convert parameters to a query string and replace '+' with '%20'
         const queryString = new URLSearchParams(filteredParams).toString().replace(/\+/g, '%20');
         return `${base}?${queryString}`;
     }
 
-    // Construct the URL with the provided parameters.
     let url = constructURL(baseURL, {
-        transient_name: transientName,
+        transient_name: coordinates.text,
         object_type: physicalPhenomena,
-        messenger_type: messengerType
+        messenger_type: messengerType,
+        radius: coordinates.ang,
+        ra: coordinates.ra,
+        dec: coordinates.dec
     });
 
     console.log(url);  // Debug: Log the constructed URL.
 
-    // Make the API request.
+    // Make the API request
     return fetch(url, { headers })
         .then(response => {
             if (!response.ok) {
