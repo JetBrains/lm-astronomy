855d7a995f15129b4f35f08800e6ead2d91a6cf9
Sergo <serg@jetbrains.com>
2023-11-02 23:02:09 +0100

    Update fetch functions and message ID presentation.
    Reorganized the fetch functions in apiServices.js to use a single fetchPublishers function, thereby reducing code duplication and enhancing maintainability. Also updated how the message ID is derived based on the provider, leading to improved display in MessagePage. Enhanced the pagination display, and revamped the code for generating tag lists for active messages, adding flexibility and ensuring display only when the active message is defined.

--- src/front/src/api/apiServices.js
+++ src/front/src/api/apiServices.js
@@ -1,66 +1,17 @@
 // apiServices.js
 
-const BASE_URL = 'https://lm-astronomy.labs.jb.gg/api/search/';
+const BASE_URL = 'https://lm-astronomy.labs.jb.gg/api';
 const HEADERS = {
     'accept': 'application/json'
 };
-
-
-export const fetchAtelMessage = async (id) => {
-    const url = `/atel/?rss+${id}`;
-    const response = await fetch(url);
-    const text = await response.text();
-
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(text, "text/xml");
-
-    const channel = xmlDoc.getElementsByTagName('channel')[0];
-    const items = xmlDoc.getElementsByTagName('item'); // получаем все элементы 'item'
-
-
-    if (!channel || !items) {
-        throw new Error("XML does not contain expected elements.");
-    }
-
-    const messages = Array.from(items).map(item => ({
-        id : id,
-        channelTitle: channel.getElementsByTagName('title')[0].textContent,
-        channelDescription: channel.getElementsByTagName('description')[0].textContent,
-        channelLink: channel.getElementsByTagName('link')[0].textContent,
-        title: item.getElementsByTagName('title')[0].textContent,
-        description: item.getElementsByTagName('description')[0].textContent,
-        date: item.getElementsByTagName('dc:date')[0].textContent,
-        link: item.getElementsByTagName('link')[0].textContent,
-        creator: item.getElementsByTagName('creator')[0].textContent,
-        provider: "atel"
-    }));
-
-    return messages;
-}
-
-
-
-export const fetchGCNMessage = async (id) => {
-    const url = `/gcn/${id}.json`;
+export const fetchPublishers = async (publisher, id) => {
+    const url = `${BASE_URL}/${publisher}/${id}/message`
     const response = await fetch(url);
     const data = await response.json();
-    const messages = [{
-        id: data.circularId,
-        channelTitle: "GCN #" + data.circularId,
-        channelDescription: "GCN Circulars: GCN #" + data.circularId,
-        channelLink: "https://gcn.nasa.gov/circulars/",
-        title: data.subject,
-        description: data.body,
-        date: formatDate(data.createdOn),
-        link: "",
-        creator: data.submitter,
-        provider: "gcn"
-    }];
-
-    return messages;
-
+    data.id = id;
+    data.provider = publisher
+    return data;
 }
-
 function constructURL(base, params) {
     const filteredParams = Object.entries(params)
         .filter(([key, value]) => value !== null && value !== undefined);
@@ -79,7 +30,6 @@ function arrayMixer(atel, gcn) {
     // Combine the sliced arrays
     return [...slicedAtel, ...slicedGcn];
 }
-
 function formatDate(timestamp) {
     // Create a Date object from the timestamp
     const date = new Date(timestamp);
@@ -100,7 +50,6 @@ function formatDate(timestamp) {
     // Construct the formatted string
     return `${day} ${month} ${year}; ${hours}:${minutes} UT+2`;
 }
-
 function mergeDataWithMessages(dataArray, messagesArray) {
     return dataArray.map(dataItem => {
         // Find the corresponding message for this data item
@@ -122,7 +71,7 @@ export function searchAPI(objectName, ra, dec, ang, physicalPhenomena, messenger
     const ITEMS_PER_PAGE = 10;
     const coordinatesString = (ra && dec) ? `${ra} ${dec}` : '';
 
-    const url = constructURL(BASE_URL, {
+    const url = constructURL( `${BASE_URL}/search/`, {
         object_name: objectName,
         object_type: physicalPhenomena,
         messenger_type: messengerType,
@@ -145,11 +94,11 @@ export function searchAPI(objectName, ra, dec, ang, physicalPhenomena, messenger
             const atelData = Object.values(data.atel || {}).slice(startIdx, endIdx);
             const gcnData = Object.values(data.gcn || {}).slice(startIdx, endIdx);
 
-            const atelDataPromises = atelData.flat().map(item => fetchAtelMessage(item.record_id));
-            const gcnDataPromises = gcnData.flat().map(item => fetchGCNMessage(item.record_id));
+            const atelDataPromises = atelData.flat().map(item => fetchPublishers("atel",item.record_id));
+            const gcnDataPromises = gcnData.flat().map(item => fetchPublishers("gcn", item.record_id));
 
-            const atel = (await Promise.all(atelDataPromises)).flat();
-            const gcn = (await Promise.all(gcnDataPromises)).flat();
+            const atel = (await Promise.all(atelDataPromises));
+            const gcn = (await Promise.all(gcnDataPromises));
 
             const atelMessages = mergeDataWithMessages(atelData.flat(), atel);
             const gcnMessages = mergeDataWithMessages(gcnData.flat(), gcn);
@@ -157,3 +106,25 @@ export function searchAPI(objectName, ra, dec, ang, physicalPhenomena, messenger
             return arrayMixer(atelMessages, gcnMessages);
         });
 }
+
+
+// const c = {
+//     "author": "P. Balanutsa, V. Lipunov, D. Buckley, E. Gorbovskoy, V. Kornilov, N. Tiurina, A. Kuznetsov, D. Vlasenko, V. Vladimirov, D. Zimnukhov, A. Chasovnikov, N. Budnev, O. Gress, R. Rebolo Lopez, M. Serra-Ricart, R. Podesta, F. Podesta, C. Lopez, C. Francile, H. Levato, V. Senik, A. Tlatov, D. Dormidontov, A. Gabovich, V. Yurkov, D. Kobtsev, Y. Sergienko, T. Pogrosheva, V. Shumkov",
+//     "link": "https://www.astronomerstelegram.org/?read=12345",
+//     "title": "ATel 12345: MASTER PSN detection in UGC03660",
+//     "creator": "P. Balanutsa, V. Lipunov, D. Buckley, E. Gorbovskoy, V. Kornilov, N. Tiurina, A. Kuznetsov, D. Vlasenko, V. Vladimirov, D. Zimnukhov, A. Chasovnikov, N. Budnev, O. Gress, R. Rebolo Lopez, M. Serra-Ricart, R. Podesta, F. Podesta, C. Lopez, C. Francile, H. Levato, V. Senik, A. Tlatov, D. Dormidontov, A. Gabovich, V. Yurkov, D. Kobtsev, Y. Sergienko, T. Pogrosheva, V. Shumkov",
+//     "date": "2018-12-31T17:06:40+00:00",
+//     "description": "MASTER OT J070634.76+635056.9 discovery - PSN in UGC03660    MASTER-Kislovodsk auto-detection system discovered OT source at (RA, Dec) = 07h 06m 34.76s +63d 50m 56.9s on 2017-12-30.7916 UT. ...",
+//     "creatoremail": "tiurina2009@gmail.com",
+//     "identifier": "ATel12345",
+//     "provenance": "Credentialed by: Nataly Tyurina (tiurina@sai.msu.ru)",
+//     "publisher": "The Astronomer's Telegram"
+// }
+//
+// const b = {
+//     "author": "Frank Marshall at GSFC  <marshall@khamseen.gsfc.nasa.gov>",
+//     "link": "https://gcn.nasa.gov/circulars/138.json",
+//     "title": "RXTE Observation of GRB 980706",
+//     "date": "1998-07-08T03:51:42",
+//     "description": "F. E. Marshall (NASA/GSFC) and T. Takeshima (USRA/GSFC),\non behalf of the RXTE team, and P. Woods (UAH/MSFC) on\nbehalf of the BATSE team, report the detection\nof an X-ray source during scans of the Locburst error region for\nGRB 980706 (BATSE trigger 6904) with the PCA on RXTE.\nA 4x8 degree region centered\non RA=161.82 and DEC=57.52 was scanned during a 2-hour\ninterval starting July 6.781, which is 2.7 hours after the burst.\nA 90% confidence error region for the source has corners at\n(RA, DEC) of (165.586, 57.801), (164.897, 55.836),\n(164.366, 55.890), and (164.993, 57.860).\nSince this error box intersects the preliminary IPN annulus\n(GCN Circ. 129), the X-ray source may be the\nafterglow of the GRB. If so, the afterglow had a flux \nof about 2 mCrab in the 2-10 keV band 2.9 hours after the burst.\nThe intersection of the error box and the preliminary IPN annulus\nis centered at (165.137, 57.385),and has a width of about 0.6 deg.\nin RA and 0.026 deg. in DEC. However, the X-ray source\nwas not detected during two subsequent scans 1.5 hours later\nindicating either unusual variability (a decay index of\nat least 3 for an assumed power-law decay) or that the X-ray source\nis not located in the intersection of the error box and the annulus.\nThe ROSAT-survey source J105825.9+564716, which lies\njust outside the X-ray error box, is\nan alternative identification of the X-ray source.\nIn this case, the X-ray source is unrelated to the GRB."
+// }
--- src/front/src/pages/MessagePage/MessagePage.js
+++ src/front/src/pages/MessagePage/MessagePage.js
@@ -11,30 +11,14 @@
             messagesData && messagesData.length > 0 ? messagesData[0].record_id : null
         );
         const activeMessage = messagesData && messagesData.find(msg => msg.record_id === activeMessageId);
-
-
         const handleCardClick = (id) => {
             setActiveMessageId(id);
             // Здесь можно вызывать функцию для перезагрузки контента в main колонке, когда она будет готова
         };
-
-        function extractChannelID(channelTitle = '') {
-            // Убираем начальные и конечные пробелы и знаки препинания
-            let trimmedTitle = channelTitle.trim().replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, '');
-
-            // Извлекаем нужную часть заголовка с использованием регулярного выражения
-            let matched = trimmedTitle.match(/ATel #\d+/);
-
-            return matched ? matched[0] : 'Unknown';
-            return channelTitle;
-        }
-
         function trimChannelIdFromTitle(title='') {
             let trimmedTitle = title.trim().replace(/^ATel \d+:\s*/, '');
             return trimmedTitle.trim();
-            return title;
         }
-
         function formatDate(inputDate) {
             const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             const date = new Date(inputDate);
@@ -63,13 +47,50 @@
 
             return `${day} ${month} ${year}; ${hours}:${minutes} ${timeLabel}`;
         }
+        function generateTagList(activeMessage) {
+            // Пустой массив для хранения элементов li
+            const listItems = [];
+
+
+            const addItem = (item) => {
+                if (Array.isArray(item) && item.length > 0) {
+                    item.forEach(value => {
+                        const trimmedValue = value.trim();
+                        if (trimmedValue) {
+                            listItems.push(<li key={value}>{value}</li>);
+                        }
+                    });
+                } else if (typeof item === 'string' && item.trim()) {
+                    listItems.push(<li key={item}>{item}</li>);
+                }
+            };
 
 
 
+            if (activeMessage) {
+                addItem(activeMessage.object_name);
+                addItem(activeMessage.messenger_type);
+                addItem(activeMessage.object_type);
+                addItem(activeMessage.event_type);
+            }
 
+            return listItems;
+        }
 
+        function ActiveMessageTagList({ activeMessage }) {
+            // Проверка, что activeMessage не undefined или null
+            if (!activeMessage) {
+                return null;
+            }
 
-        // console.log(messagesData);
+            const listItems = generateTagList(activeMessage);
+            return (
+                <ul>
+                    {listItems.length > 0 ? listItems : <li>No data available</li>}
+                </ul>
+            );
+        }
+
 
         if (!messagesData) {
             return <div>Loading...</div>;
@@ -80,56 +101,46 @@
                 <div className="container">
                     <Header />
                     <CollapsedSearchPanel />
-                    <div className="columns">
-                    <div className="sidemenu">
-                        <ul className="cards-list">
-                            {messagesData && messagesData.map((msg) => (
-                                <li
-                                    key={msg.record_id}
-                                    className={`card-item ${msg.record_id === activeMessageId ? 'active-card' : ''}`}
-                                    onClick={() => handleCardClick(msg.record_id)}
-                                >
-                                    <div className="card-header">
-                                    <div className="card-date">{formatDate(msg.date)}</div>
-                                    <div className="card-id">{extractChannelID(msg.channelTitle)}</div>
-                                    </div>
-                                    <div className="card-title">{trimChannelIdFromTitle(msg.title)}</div>
-                                </li>
-                            ))}
-                        </ul>
-                        <h2>GCN</h2>
-                        <ul>
-                            {/*{messagesData.gcn && messagesData.gcn.map((msg, index) => (*/}
-                            {/*    <li key={index}>{msg.title}</li>*/}
-                            {/*))}*/}
-                        </ul>
-                        <div className="pagination">
-                            <button onClick={() => setCurrentPage(prev => Math.max(1, prev - 1))} disabled={currentPage === 1}>&#8678;</button>
-                            <span>{currentPage} из {totalPages}</span>
-                            <button onClick={() => setCurrentPage(prev => Math.min(totalPages, prev + 1))} disabled={currentPage === totalPages}>&#8680;</button>
+                        <div className="columns">
+                        <div className="sidemenu">
+                            <ul className="cards-list">
+                                {messagesData && messagesData.map((msg) => (
+                                    <li
+                                        key={msg.record_id}
+                                        className={`card-item ${msg.record_id === activeMessageId ? 'active-card' : ''}`}
+                                        onClick={() => handleCardClick(msg.record_id)}
+                                    >
+                                        <div className="card-header">
+                                        <div className="card-date">{formatDate(msg.date)}</div>
+                                            <div className="card-id">
+                                                {msg.provider === "atel" ? `ATel${msg.record_id}` : null}
+                                                {msg.provider === "gcn" ? `GCN${msg.record_id.split('.')[0]}` : null}
+                                            </div>
+
+                                        </div>
+                                        <div className="card-title">{trimChannelIdFromTitle(msg.title)}</div>
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className="pagination">
+                                <button onClick={() => setCurrentPage(prev => Math.max(1, prev - 1))} disabled={currentPage === 1}>&#8678;</button>
+                                <span>{currentPage} из {totalPages}</span>
+                                <button onClick={() => setCurrentPage(prev => Math.min(totalPages, prev + 1))} disabled={currentPage === totalPages}>&#8680;</button>
+                            </div>
                         </div>
-                    </div>
                         <div className="main">
-                            {activeMessage && (
-                                <>
-                                    <h2 className="main-card-title">{trimChannelIdFromTitle(activeMessage.title)}</h2>
-                                    <div>
-                                        <strong>Object Name:</strong> {activeMessage.object_name.join(', ')}
-                                    </div>
-                                    <div>
-                                        <strong>Event Type:</strong> {activeMessage.event_type.join(', ')}
-                                    </div>
-                                    <div>
-                                        <strong>Messenger Type:</strong> {activeMessage.messenger_type.join(', ')}
-                                    </div>
-                                    <div>
-                                        <strong>Object Type:</strong> {activeMessage.object_type.join(', ')}
-                                    </div>
-                                    <p>{activeMessage.description}</p>
-                                    <p>{activeMessage.creator}</p>
-                                </>
-                            )}
-                        </div>
+                                {activeMessage ? (
+                                    <>
+                                        <ActiveMessageTagList activeMessage={activeMessage} />
+
+                                        <h2 className="main-card-title">{trimChannelIdFromTitle(activeMessage.title)}</h2>
+                                        <p>{activeMessage.description}</p>
+                                        <p>{activeMessage.creator}</p>
+                                    </>
+                                 ) : (
+                                    <p>No message selected</p>
+                                    )}
+                            </div>
                     </div>
                 </div>
             </div>
