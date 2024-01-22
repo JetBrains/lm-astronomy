e521a72ee5e52d79adab4b3e93ebf964a0c0ea1b
Sergo <serg@jetbrains.com>
2024-01-22 15:46:29 +0100

    Adjust CSS and clean up HomePage.js
    This commit refines the UI across multiple components by adjusting padding, margin sizes, and other CSS properties. The code in HomePage.js was streamlined by removing commented-out debug code that was no longer needed. Fine-tuning these details enhances the user interface and overall UX.

--- src/front/src/pages/HomePage.js
+++ src/front/src/pages/HomePage.js
@@ -12,18 +12,6 @@ import messengerType from "../components/MessengerType/MessengerType";
 
 function HomePage() {
 
-    // const { transientName, ra, dec, ang, physicalObject, eventType, messengerType} = useContext(SearchParamsContext);
-    // if(transientName || ra || dec || ang || physicalObject || messengerType || eventType) {
-    //     console.log("transientName = ", transientName);
-    //     console.log("ra = ", ra);
-    //     console.log("dec = ", dec);
-    //     console.log("ang = ", ang);
-    //     console.log("physicalObject = ", physicalObject);
-    //     console.log("eventType = ", eventType);
-    //     console.log("messengerType = ", messengerType);
-    // }
-
-
     return (
         <div className="app-container font-base">
             <div className="container">
