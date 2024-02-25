4bf5b0c557b56575a9958b16717be001aba1d24c
Sergo <serg@jetbrains.com>
2024-02-25 23:34:55 +0100

    Refactor code for better readability and remove unnecessary comments
    The MessagePage.js and api services files have been extensively refactored for better readability and consistency. Redundant comments and white spaces were removed. URL construction in fetchDataFromSource function is also updated for better adaptability across different environments. Moreover, certain data processing functions have been simplified for better performance. This refactor does not add any new functionality but makes the existing code cleaner and

--- src/front/src/api/loadRecords.js
+++ src/front/src/api/loadRecords.js
@@ -20,17 +20,19 @@ function normalizeGCNData(gcnData) {
     if (!Array.isArray(gcnData)) {
         gcnData = [gcnData];
     }
+
     return gcnData.map(item => ({
         author: item.submitter,
         creator: item.submitter,
         creatorEmail: item.email,
-        date: item.createdOn, // convert to string
+        date: item.createdOn,
         description: item.body,
         identifier: `GCN${item.circularId}`,
         link: `https://gcn.nasa.gov/circulars/${item.circularId}.json`,
         title: item.subject
     }));
 }
+
 const fetchRecords = async (nimRecords, pageNumber = 1, recordsPerPage = 10) => {
     const startIndex = (pageNumber - 1) * recordsPerPage;
     const selectedNimRecords = nimRecords.slice(startIndex, startIndex + recordsPerPage);
@@ -38,7 +40,6 @@ const fetchRecords = async (nimRecords, pageNumber = 1, recordsPerPage = 10) => 
     const atelPromises = selectedNimRecords
         .filter(item => item.provider === 'atel')
         .map(item => fetchDataFromSource(item.record_id, true));
-
     const gcnPromises = selectedNimRecords
         .filter(item => item.provider === 'gcn')
         .map(item => fetchDataFromSource(item.record_id, false));
@@ -50,53 +51,45 @@ const fetchRecords = async (nimRecords, pageNumber = 1, recordsPerPage = 10) => 
 const mergeDataWithRecords = (nimRecords, fetchedRecords) => {
     return nimRecords.filter(nimItem => {
         if (nimItem.provider === 'atel') {
-            return fetchedRecords.some(record =>
-                record && record.provider === 'atel' && `ATel${nimItem.record_id}` === record.identifier
-            );
+            return fetchedRecords.some(record => record && record.provider === 'atel' && `ATel${nimItem.record_id}` === record.identifier);
         } else if (nimItem.provider === 'gcn') {
-            return fetchedRecords.some(record =>
-                record && record.provider === 'gcn' && `GCN${nimItem.record_id.split(".")[0]}` === record.identifier
-            );
+            return fetchedRecords.some(record => record && record.provider === 'gcn' && nimItem.record_id === record.identifier);
         }
         return false;
     })
         .map(nimItem => {
-            const correspondingRecord = fetchedRecords.find(record =>
-                (nimItem.provider === 'atel' && `ATel${nimItem.record_id}` === record.identifier) ||
-                (nimItem.provider === 'gcn' && `GCN${nimItem.record_id.split(".")[0]}` === record.identifier)
-            );
-            return { ...nimItem, ...correspondingRecord };
+
+            const correspondingRecord = fetchedRecords.find(record => (nimItem.provider === 'atel' && `ATel${nimItem.record_id}` === record.identifier) || (nimItem.provider === 'gcn' && nimItem.record_id === record.identifier));
+            return {...nimItem, ...correspondingRecord};
         });
 };
-
-
 
 
 const fetchDataFromSource = async (recordId, isAtel = true) => {
     try {
         const url = isAtel
-            ? `/atel/?rss+${recordId}`
-            : `/gcn/${recordId.replace('neg', '-').replace('.gcn3', '')}.json`;
+            ? `https://lm-astronomy.labs.jb.gg/api/atel/${recordId}/message`
+            : `https://lm-astronomy.labs.jb.gg/api/gcn/${recordId}/message`;
         const response = await fetch(url);
         if (response.status !== 200) {
             throw new Error('Failed to fetch data');
         }
         if (isAtel) {
-            const responseXml = await response.text();
-            const xmlDoc = new DOMParser().parseFromString(responseXml, 'text/xml');
-            const result = processXmlToJson(xmlDoc);
-            return result.map(record => ({ ...record, provider: 'atel' })); // Добавляем provider
+            const responseData = await response.json();
+            // const responseXml = await response.text();
+            // const xmlDoc = new DOMParser().parseFromString(responseXml, 'text/xml');
+            // const result = processXmlToJson(xmlDoc);
+            return [responseData].map(record => ({...record, provider: 'atel'}));
         } else {
             const responseData = await response.json();
-            const result = normalizeGCNData(responseData);
-            return result.map(record => ({ ...record, provider: 'gcn' })); // Добавляем provider
+            // const result = normalizeGCNData(responseData);
+            return [responseData].map(record => ({...record, identifier: recordId, provider: 'gcn'}));
         }
     } catch (error) {
         console.error('Failed to load record:', error);
         return null;
     }
 };
-
 
 
 export const loadDataAndMerge = async (nimRecords, page) => {
