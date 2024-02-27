1c4cd37e5103623e00b225f0fa37298749238494
Sergo <serg@jetbrains.com>
2024-02-27 01:09:05 +0100

    Update SVG search icons in UI components
    This commit modifies the size, viewBox and certain elements of SVG search icons within `CollapsedSearchPanel` and `SearchButton` components. The changes improve the visual balance of the SVG and simplify the code by removing unused SVG elements within `SearchButton` component.

--- src/front/src/components/SearchButton/SearchButton.js
+++ src/front/src/components/SearchButton/SearchButton.js
@@ -8,35 +8,16 @@ function SearchButton({onSearch, loading, disabled}) {
         <div className={`search-button ${loading ? 'loading' : ''} ${disabled ? 'disabled' : ''}`} onClick={onSearch}>
             <span className="search-text">SEARCH</span>
             <div className="icons-container">
-                <svg className="search-icon" width="84" height="84" viewBox="0 0 84 84" fill="none"
+                <svg width="90" height="70" viewBox="0 0 108 84" fill="none"
                      xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="42" cy="42" r="40.5" stroke="black" strokeWidth="3"/>
+                    <circle cx="54" cy="42" r="40.5" stroke="black" stroke-width="3"/>
                     <path
-                        d="M82.5 42.8566C82.5 49.4991 78.241 55.7313 70.8965 60.3778C63.5709 65.0124 53.3584 67.928 42 67.928C30.6416 67.928 20.4291 65.0124 13.1035 60.3778C5.75899 55.7313 1.5 49.4991 1.5 42.8566C1.5 36.214 5.75899 29.9819 13.1035 25.3354C20.4291 20.7008 30.6416 17.7852 42 17.7852C53.3584 17.7852 63.5709 20.7008 70.8965 25.3354C78.241 29.9819 82.5 36.214 82.5 42.8566Z"
-                        stroke="black" strokeWidth="3"/>
+                        d="M106.5 42C106.5 49.1898 101.05 56.042 91.4741 61.1846C81.9602 66.2939 68.7138 69.5 54 69.5C39.2863 69.5 26.0398 66.2939 16.5259 61.1846C6.95 56.042 1.5 49.1898 1.5 42C1.5 34.8102 6.95 27.958 16.5259 22.8154C26.0398 17.7061 39.2863 14.5 54 14.5C68.7138 14.5 81.9602 17.7061 91.4741 22.8154C101.05 27.958 106.5 34.8102 106.5 42Z"
+                        stroke="black" stroke-width="3"/>
                     <circle className={`pupil ${loading ? "pupil-spinning" : ""}`} cx="32.0001" cy="36.0006" r="8.78571"
                             stroke="black" strokeWidth="3"/>
                 </svg>
 
-                <svg className="search-icon" width="84" height="84" viewBox="0 0 84 84" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="42" cy="42" r="40.5" stroke="black" strokeWidth="3"/>
-                    <path
-                        d="M82.5 42.8566C82.5 49.4991 78.241 55.7313 70.8965 60.3778C63.5709 65.0124 53.3584 67.928 42 67.928C30.6416 67.928 20.4291 65.0124 13.1035 60.3778C5.75899 55.7313 1.5 49.4991 1.5 42.8566C1.5 36.214 5.75899 29.9819 13.1035 25.3354C20.4291 20.7008 30.6416 17.7852 42 17.7852C53.3584 17.7852 63.5709 20.7008 70.8965 25.3354C78.241 29.9819 82.5 36.214 82.5 42.8566Z"
-                        stroke="black" strokeWidth="3"/>
-                    <circle className={`pupil ${loading ? "pupil-spinning" : ""}`} cx="42.0001" cy="52.0006" r="8.78571"
-                            stroke="black" strokeWidth="3"/>
-                </svg>
-
-                <svg className="search-icon" width="84" height="84" viewBox="0 0 84 84" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="42" cy="42" r="40.5" stroke="black" strokeWidth="3"/>
-                    <path
-                        d="M82.5 42.8566C82.5 49.4991 78.241 55.7313 70.8965 60.3778C63.5709 65.0124 53.3584 67.928 42 67.928C30.6416 67.928 20.4291 65.0124 13.1035 60.3778C5.75899 55.7313 1.5 49.4991 1.5 42.8566C1.5 36.214 5.75899 29.9819 13.1035 25.3354C20.4291 20.7008 30.6416 17.7852 42 17.7852C53.3584 17.7852 63.5709 20.7008 70.8965 25.3354C78.241 29.9819 82.5 36.214 82.5 42.8566Z"
-                        stroke="black" strokeWidth="3"/>
-                    <circle className={`pupil ${loading ? "pupil-spinning" : ""}`} cx="52.0001" cy="36.0006" r="8.78571"
-                            stroke="black" strokeWidth="3"/>
-                </svg>
             </div>
         </div>);
 }
